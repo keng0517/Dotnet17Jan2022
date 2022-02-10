73270d852ca6c7b8d7651e5f1c0206919b66c70a
unknown <keng051700@gmail.com>
2022-02-10 23:57:06 +0800

    update Day16 pizzaSolution

--- Day16/PizzaSolution/PizzaApplication/Program.cs
+++ Day16/PizzaSolution/PizzaApplication/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaApplication.Models;
+using PizzaApplication.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<IRepo<int, Pizza>, PizzaEFRepo>();
+string conn = builder.Configuration.GetConnectionString("conn");
+builder.Services.AddDbContext<PizzaShopContext>(options =>
+{
+    options.UseSqlServer(conn);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -10,7 +20,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
-app.UseStaticFiles();
+app.UseStaticFiles(); 
 
 app.UseRouting();
 
