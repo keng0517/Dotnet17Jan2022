811721e440fad3a029c6bf0074c905f04b44007b
unknown <keng051700@gmail.com>
2022-02-14 22:40:51 +0800

    update day 18

---
+++ Day18/SampleMVCTogetherApp/SampleMVCTogetherApp/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleMVCTogetherApp.Models
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
